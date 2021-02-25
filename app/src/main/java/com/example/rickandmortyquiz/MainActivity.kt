da3e62738a5f150a8cad6ce57af74c590096b4b9
Anastasiia Egorova <egorova.an94@gmail.com>
2021-02-25 12:38:57 -0400

    Drawer menu done

--- app/src/main/java/com/example/rickandmortyquiz/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortyquiz/MainActivity.kt
@@ -2,16 +2,28 @@ package com.example.rickandmortyquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.example.rickandmortyquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+    //private lateinit var drawerLayout: DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         val navController = this.findNavController(R.id.myNavHostFragment)
+
+        //drawerLayout = binding.drawerLayout
+        //NavigationUI.setupWithNavController(binding.navView, navController)
         NavigationUI.setupActionBarWithNavController(this, navController)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
